9ee01c3baeaa782a7aabc404aab5906fec57714f
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2013-11-26 10:44:38 -0500

    try to recover from disconnections

--- xml_tools/lookup_from_db.py
+++ xml_tools/lookup_from_db.py
@@ -1,24 +1,35 @@
 import MySQLdb
 
-conn = None
-cursor = None
+
+class DB(object):
+    def __init__(*args):
+        self.args = args
+        self.conn = None
+
+    def connect(self):
+        self.conn = MySQLdb.connect(*args)
+
+    def query(self, sql):
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(sql)
+        except (AttributeError, MySQLdb.OperationalError):
+            self.connect()
+            cursor = self.conn.cursor()
+            cursor.execute(sql)
+        return cursor
+
+db = DB('ptilouis', 'biophenics', 'biophenics', 'biophenics', use_unicode=True)
 platelist = None
 platedir_to_plateid = {}
-
-def init():
-    global conn, cursor
-    if conn is None:
-        conn = MySQLdb.connect('ptilouis', 'biophenics', 'biophenics', 'biophenics', use_unicode=True)
-        cursor = conn.cursor()
 
 def digits(s):
     return filter(lambda c: c.isdigit(), s)
 
 def get_plateid(platedir):
     global platelist
-    init()
     if platelist is None:
-        cursor.execute('SELECT PLATE_ID, PLATE_CODE FROM PLATE WHERE PLATE_CODE IS NOT NULL')
+        cursor = db.execute('SELECT PLATE_ID, PLATE_CODE FROM PLATE WHERE PLATE_CODE IS NOT NULL')
         plate_ids, plate_codes = zip(*cursor.fetchall())
         plate_codes = [digits(pc) for pc in plate_codes]
         platelist = [(pc, pid) for pc, pid in zip(plate_codes, plate_ids) if len(pc) >= 8]
@@ -41,34 +52,31 @@ def get_plateid(platedir):
 
 def lookup_genes(platedir, wellrow, wellcol, target_sequence=False, concentration=False):
     assert concentration == False
-    init()
     if not target_sequence:
-        cursor.execute('''SELECT DUPLEX_NAME, DUPLEX_NUMBER FROM SI_RNA JOIN SI_RNA_WELL USING (SI_RNA_ID)
-                      JOIN WELL USING (WELL_ID) JOIN PLATE USING (PLATE_ID) WHERE PLATE_ID=%d and WELL_ROW=%s and WELL_COL=%s''' %
-                       (get_plateid(platedir), wellrow, wellcol))
+        cursor = db.execute('''SELECT DUPLEX_NAME, DUPLEX_NUMBER FROM SI_RNA JOIN SI_RNA_WELL USING (SI_RNA_ID)
+          JOIN WELL USING (WELL_ID) JOIN PLATE USING (PLATE_ID) WHERE PLATE_ID=%d and WELL_ROW=%s and WELL_COL=%s''' %
+          (get_plateid(platedir), wellrow, wellcol))
         
         return ','.join(['%s#%d' % (g, n) for g, n in cursor.fetchall()])
     else:
-        cursor.execute('''SELECT DUPLEX_NAME, DUPLEX_NUMBER, TARGET_SEQUENCE FROM SI_RNA JOIN SI_RNA_WELL USING (SI_RNA_ID)
-                      JOIN WELL USING (WELL_ID) JOIN PLATE USING (PLATE_ID) WHERE PLATE_ID=%d and WELL_ROW=%s and WELL_COL=%s''' %
-                       (get_plateid(platedir), wellrow, wellcol))
+        cursor = db.execute('''SELECT DUPLEX_NAME, DUPLEX_NUMBER, TARGET_SEQUENCE FROM SI_RNA JOIN SI_RNA_WELL USING (SI_RNA_ID)
+          JOIN WELL USING (WELL_ID) JOIN PLATE USING (PLATE_ID) WHERE PLATE_ID=%d and WELL_ROW=%s and WELL_COL=%s''' %
+          (get_plateid(platedir), wellrow, wellcol))
         vals = cursor.fetchall()
         return ','.join([('%s#%d' % (g, n)) for g, n, ts in vals]), ','.join([ts for g, n, ts in vals])
 
 
 def lookup_chemicals(platedir, wellrow, wellcol, target_sequence=False, concentration=False):
     assert target_sequence == False
-    init()
-    cursor.execute('''SELECT CHEMICAL_NAME, CONCENTRATION_VALUE, UNIT_NAME FROM CHEMICAL JOIN CHEMICAL_WELL USING (CHEMICAL_ID)
-                      JOIN UNIT ON UNIT.UNIT_ID = CONCENTRATION_UNIT_ID
-                      JOIN WELL USING (WELL_ID) JOIN PLATE USING (PLATE_ID) WHERE PLATE_ID=%d and WELL_ROW=%s and WELL_COL=%s''' %
-                   (get_plateid(platedir), wellrow, wellcol))
+    cursor = db.execute('''SELECT CHEMICAL_NAME, CONCENTRATION_VALUE, UNIT_NAME FROM CHEMICAL JOIN CHEMICAL_WELL USING (CHEMICAL_ID)
+      JOIN UNIT ON UNIT.UNIT_ID = CONCENTRATION_UNIT_ID
+      JOIN WELL USING (WELL_ID) JOIN PLATE USING (PLATE_ID) WHERE PLATE_ID=%d and WELL_ROW=%s and WELL_COL=%s''' %
+      (get_plateid(platedir), wellrow, wellcol))
     v = cursor.fetchall()
     return ', '.join(['%s' % c for c, _, _ in v]), ', '.join(unicode(str(con)) + ' ' + un.decode('latin-1') for _, con, un in v)
 
 
 def lookup_treatment(t):
-    init()
     if t == 'Genes':
         return lookup_genes
     if t == 'Chemicals':
